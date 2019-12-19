8891e2d8b7cf6869132accf122599f79c155f76b
Trung Tinh Pham <TrungTinh.Pham@broadcom.com>
2019-12-19 11:49:06 +0700

    Config build and remove redundant services

--- hr-service/src/main/java/com/tinhpt/hr/repositories/DepartmentDao.java
+++
@@ -1,8 +0,0 @@
-package com.tinhpt.hr.repositories;
-
-import com.tinhpt.hr.entities.DepartmentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentDao extends JpaRepository<DepartmentEntity, Long> {
-    
-}
--- hr-service/src/main/java/com/tinhpt/hr/services/IFakerService.java
+++
@@ -1,8 +0,0 @@
-package com.tinhpt.hr.services;
-
-public interface IFakerService {
-    void createTeleSale();
-    void createSale();
-    void createSaleAdmin();
-    void createSaleManager();
-}
